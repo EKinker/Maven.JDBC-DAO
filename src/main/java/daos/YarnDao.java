347da828f9bc30ff75d27475d4d7cdedf466e481
Liz Kinker <Liz.Kinker@gmail.com>
2023-08-27 16:19:55 -0400

    I don't know what this is doing, but I hope it works

--- src/main/java/daos/YarnDao.java
+++ src/main/java/daos/YarnDao.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface YarnDao {
     Yarn getYarn(int id);
     List<Yarn> getAllYarn();
-    List<Yarn> getYarnByBrand(String brand);
+    //List<Yarn> getYarnByBrand(String brand);  //Try this later
     boolean addYarn(Yarn yarn);
     boolean updateYarn(Yarn yarn);
     boolean deleteYarn(int id);
--- src/main/java/daos/YarnStuff.java
+++ src/main/java/daos/YarnStuff.java
@@ -1,12 +1,11 @@
 package daos;
 
+import com.sun.jdi.connect.Connector;
 import database.ConnectionFactory;
 import models.Yarn;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class YarnStuff implements YarnDao {
@@ -44,26 +43,80 @@ public class YarnStuff implements YarnDao {
 
     @Override
     public List<Yarn> getAllYarn() {
-        return null;
-    }
+        Connection connection = ConnectionFactory.getConnection();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Yarn");
 
-    @Override
-    public List<Yarn> getYarnByBrand(String brand) {
+            List<Yarn> yarns = new ArrayList<>();
+
+            while (rs.next()) {
+                Yarn yarn = getYarnResultSet(rs);
+                yarns.add(yarn);
+            }
+            return yarns;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
         return null;
     }
 
     @Override
     public boolean addYarn(Yarn yarn) {
+        Connection connection = ConnectionFactory.getConnection();
+        try {
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO Yarn VALUES (NULL,?,?,?,?,?,?,?)");
+            ps.setString(1, yarn.getBrand());
+            ps.setString(2, yarn.getColor());
+            ps.setString(3, yarn.getFiberType());
+            ps.setString(4, yarn.getWeight());
+            ps.setInt(5, yarn.getYardage());
+            ps.setDouble(6, yarn.getPrice());
+            ps.setInt(7, yarn.getQuantity());
+            int i = ps.executeUpdate();
+
+            if (i == 1) return true;
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
         return false;
     }
 
     @Override
     public boolean updateYarn(Yarn yarn) {
+        Connection connection = ConnectionFactory.getConnection();
+        try {
+            PreparedStatement ps = connection.prepareStatement("UPDATE Yarns SET Brand=?,Color=?,FiberType=?,WeightCategory=?,Yardage=?,Price=?,Quantity=? WHERE YarnID=?");
+            ps.setString(1, yarn.getBrand());
+            ps.setString(2, yarn.getColor());
+            ps.setString(3, yarn.getFiberType());
+            ps.setString(4, yarn.getWeight());
+            ps.setInt(5, yarn.getYardage());
+            ps.setDouble(6, yarn.getPrice());
+            ps.setInt(7, yarn.getQuantity());
+            int i = ps.executeUpdate();
+
+            if (i == 1) return true;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
         return false;
     }
 
     @Override
     public boolean deleteYarn(int id) {
+        Connection connection = ConnectionFactory.getConnection();
+        try {
+            Statement stmt = connection.createStatement();
+            int i = stmt.executeUpdate("DELETE FROM Yarn WHERE YarnID=" + id);
+            if (i == 1) return true;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
         return false;
     }
 }
